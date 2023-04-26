8ee207489bea22c6c4b03136fe286bb767af1886
Tom Ko <yungsu2391@gmail.com>
2023-04-26 09:45:12 +0900

    nestjs crawler

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  root() {
+    console.log('root');
+    return { message: 'ejs 연결완료' };
+  }
+}
